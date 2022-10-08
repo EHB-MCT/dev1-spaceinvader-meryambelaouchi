44b75dea5ea5e75f66aacfea8f80c2a5846e752e
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-08 17:36:01 +0200

    background

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,16 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSpace();
+
+function drawSpace() {
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fill();
+
+}
