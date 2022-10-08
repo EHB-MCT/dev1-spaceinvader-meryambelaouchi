1ece36a4655f0c57d2411ff8e6e0e500a69724d6
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2022-10-08 17:44:42 +0200

    Space Invader Done

--- spaceInvader.js
+++ spaceInvader.js
@@ -12,13 +12,16 @@ function drawSpace() {
     context.beginPath();
     context.rect(50, 50, 300, 300);
     context.fill();
-    
-    context.fillStyle = 'green';
+
+    context.fillStyle = 'Lime';
     context.beginPath();
     context.rect(125, 75, 50, 50);
     context.rect(225, 75, 50, 50);
     context.rect(125, 125, 150, 200);
-    
+    context.rect(75, 125, 50, 50);
+    context.rect(75, 225, 50, 50);
+    context.rect(275, 125, 50, 50);
+    context.rect(275, 225, 50, 50);
     context.fill();
 
 }
